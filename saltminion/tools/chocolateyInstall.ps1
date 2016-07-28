e9edec80d408d829225e404104916072b48e8487
Alex Clay <aclay@mac.com>
2016-07-27 23:09:56 -0400

    Updating to salt 2016.3.1. Re-worked the installation to download the installer and then run as a separate process. This allows the GUI installer to complete in silent mode before returning control to chocolately. Updated the icon to a cdn-cached transparent PNG per the recommendation of the reviewer of the previous package update. Added an explicit version to the 2008 vcredist dependency per the recommendation by the reviewer of of the previous pagage update. Added myself to the owners tag and updated the source to my fork on GitHub since the original project seems inactive and Pat O'Shea indicated he doesn't want to maintain this package anymore.

--- saltminion/tools/chocolateyInstall.ps1
+++ saltminion/tools/chocolateyInstall.ps1
@@ -1,7 +1,37 @@
 ﻿$packageName = 'saltminion'
-$installerType = 'EXE' 
-$url = 'http://docs.saltstack.com/downloads/Salt-Minion-2015.5.3-x86-Setup.exe'
-$url64 = 'http://docs.saltstack.com/downloads/Salt-Minion-2015.5.3-AMD64-Setup.exe' 
-$silentArgs = '/S' 
-$validExitCodes = @(0) 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+# Update these URLs and checksums when deploying a new version
+$url = 'https://repo.saltstack.com/windows/Salt-Minion-2016.3.1-x86-Setup.exe'
+$checksum = '4293e39ed3bfe90ac3a3a2dba66c2047'
+$url64 = 'https://repo.saltstack.com/windows/Salt-Minion-2016.3.1-AMD64-Setup.exe'
+$checksum64 = 'f3269cf009b9ecc8770dff430a5a74e0'
+
+# We need a path to download the file so we can manually update it
+$installerFilePath = "${env:chocolateyPackageFolder}\${env:chocolateyPackageName}-${env:chocolateyPackageVersion}.exe"
+
+# Ignore the downloaded installer from getting a shim
+New-Item "$installerFilePath.ignore" -type file -force | Out-Null
+
+# Get arguments to download the file from the URLs and checksums above
+$packageArgs = @{
+  packageName   = $packageName
+  fileFullPath  = $installerFilePath
+  url           = $url
+  url64bit      = $url64
+  checksum      = $checksum
+  checksumType  = 'md5'
+  checksum64    = $checksum64
+  checksumType64= 'md5'
+}
+
+# Download the file and bail if the return code is non-zero
+Get-ChocolateyWebFile @packageArgs
+
+# Install
+Write-Host "Installing ${env:chocolateyPackageName} v${env:chocolateyPackageVersion}..."
+
+# Run the downloaded file with the /S silent flag
+# We have to use Start-Process to allow the installer to finish
+# Otherwise control returns to the script too soon and the install fails
+# This is why we can't use the standard Install-ChocoletyPackage function
+Start-Process "$installerFilePath" -ArgumentList @('/S') -NoNewWindow -Wait
